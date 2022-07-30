638fcc9eaa995ed0e9fe8941949335197ace0b7e
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-07-30 21:42:08 +0530

    feat: months and days constant

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+const Weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const Months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export { Weekdays, Months };
