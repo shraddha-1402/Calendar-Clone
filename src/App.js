7b19415c611b6513d069d9984239e058ce78db39
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-07-30 21:46:09 +0530

    feat: next and prev month function

--- src/App.js
+++ src/App.js
@@ -10,19 +10,30 @@ const App = () => {
   const [firstDateTimestamp, setFirstDateTimestamp] = useState(
     getMonthsFirstDate(new Date())
   );
-
   const { month, year } = firstDateTimestamp;
+
+  const getPreviousMonth = () => {
+    const prevMonth = month === 1 ? 12 : month - 1;
+    const prevYear = month === 1 ? year - 1 : year;
+    setFirstDateTimestamp(getMonthsFirstDate(new Date(prevYear, prevMonth)));
+  };
+
+  const getNextMonth = () => {
+    const nextMonth = month === 12 ? 1 : month + 1;
+    const nextYear = month === 12 ? year + 1 : year;
+    setFirstDateTimestamp(getMonthsFirstDate(new Date(nextYear, nextMonth)));
+  };
 
   return (
     <div className="App">
       <div className="month-change-container">
-        <IconButton aria-label="chevron-left">
+        <IconButton onClick={getPreviousMonth} aria-label="chevron-left">
           <ChevronLeftIcon />
         </IconButton>
         <span className="month-display">
           {Months[month]} {year}
         </span>
-        <IconButton aria-label="chevron-right">
+        <IconButton onClick={getNextMonth} aria-label="chevron-right">
           <ChevronRightIcon />
         </IconButton>
       </div>
