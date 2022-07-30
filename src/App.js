519119464cb4e14dbae598076b1dc4eb2c8a50b8
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-07-30 21:44:28 +0530

    feat: month change component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {
-  return <div className="App"></div>;
-}
+import "./App.css";
+import { useState } from "react";
+import { IconButton } from "@mui/material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { getMonthsFirstDate } from "./utils/dateFunctions";
+import { Months } from "./constants";
+
+const App = () => {
+  const [firstDateTimestamp, setFirstDateTimestamp] = useState(
+    getMonthsFirstDate(new Date())
+  );
+
+  const { month, year } = firstDateTimestamp;
+
+  return (
+    <div className="App">
+      <div className="month-change-container">
+        <IconButton aria-label="chevron-left">
+          <ChevronLeftIcon />
+        </IconButton>
+        <span className="month-display">
+          {Months[month]} {year}
+        </span>
+        <IconButton aria-label="chevron-right">
+          <ChevronRightIcon />
+        </IconButton>
+      </div>
+    </div>
+  );
+};
 
 export default App;
