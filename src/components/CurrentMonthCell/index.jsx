385a255554b73e91e312578b47234aafdaf3e7f3
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-07-31 01:09:44 +0530

    feat: imported event create modal

--- src/components/CurrentMonthCell/index.jsx
+++ src/components/CurrentMonthCell/index.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import { EventCreateModal } from "../EventCreateModal";
+
 const CurrentMonthCell = ({ innerIndex, outerIndex, firstDateTimestamp }) => {
-  const { day } = firstDateTimestamp;
+  const [openEventModal, setOpenEventModal] = useState(false);
+  const { day, month, year } = firstDateTimestamp;
+
   return (
-    <td className="table-cell body-cell" key={innerIndex}>
+    <td
+      className="table-cell body-cell"
+      onClick={() => setOpenEventModal(true)}
+    >
       <span className="cell-date">{1 + outerIndex * 7 + innerIndex - day}</span>
+      <EventCreateModal
+        openEventModal={openEventModal}
+        setOpenEventModal={setOpenEventModal}
+        date={1 + outerIndex * 7 + innerIndex - day}
+        year={year}
+        month={month}
+      />
     </td>
   );
 };
