a7d0db4da56e34b9ed0887ee3f7f04e03cb9a5b7
shraddha-1402 <69422250+shraddha-1402@users.noreply.github.com>
2022-07-31 14:24:19 +0530

    feat: firebase project connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { firebaseConfig } from "./firebase.config";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 root.render(
   <React.StrictMode>
     <App />
